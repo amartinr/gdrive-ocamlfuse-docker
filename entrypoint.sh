7b35b64d8c250b17cff830d3b61105199ab6abd0
Abel Martín <abel.martin.ruiz@gmail.com>
2023-02-21 22:19:54 +0100

    Fix local config files inside container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
-
-# OPAM environment variables, including $PATH
 eval $(opam env)
 
-# Initialise Google OAuth
-if [[ ! -f ~/.gdfuse/config/state ]]; then
-    google-drive-ocamlfuse -headless -id ${CLIENT_ID} -secret ${SECRET}
-
-    # If authentication succesful, mount Drive on /mnt
-    if [[ "$?" == "0" ]]; then
-        google-drive-ocamlfuse -debug -log_to - /mnt
-    else
-        exit 1
-    fi
-# If state file with auth token already exists, mount Drive on /mnt
+if [[ -f ~/.gdfuse/default/config/state ]]; then
+	google-drive-ocamlfuse -debug -log_to - /mnt
 else
-    google-drive-ocamlfuse -debug -log_to - /mnt
+	google-drive-ocamlfuse -headless -id ${CLIENT_ID} -secret ${SECRET}
+
+	if [[ "$?" == "0" ]]; then
+		google-drive-ocamlfuse -debug -log_to - /mnt
+	else
+		exit 1
+	fi
 fi
