1d4d1ca073382bc9ec84f11f4980e67602d5f2f7
Abel Martín <abel.martin.ruiz@gmail.com>
2024-05-21 18:45:12 +0200

    Fix entrypoint in case state file exists without a token

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,23 @@ if [ -f "$HOME/.gdfuse/default/state" ]; then
     ACCESS_TOKEN=$(grep last_access_token "$HOME/.gdfuse/default/state" | sed -r "s/^last_access_token=//")
     if [ -n "$ACCESS_TOKEN" ]; then
         if wget "https://oauth2.googleapis.com/tokeninfo?access_token=${ACCESS_TOKEN}" -O /dev/null --quiet; then
-            printf "Access token valid. Mounting filesystem on %s.\n" "$HOME/gdrive"
+            printf "Access token still valid. Mounting filesystem on %s.\n" "$HOME/gdrive"
             google-drive-ocamlfuse -f -verbose "$HOME/gdrive"
         else
-            printf "Access token invalid. Revalidating...\n"
+            printf "Access token no longer valid. Revalidating...\n"
             rm -f "$HOME/.gdfuse/default/state"
-            if google-drive-ocamlfuse -headless -debug -log_to - -id "$CLIENT_ID" -secret "$SECRET"; then
-                google-drive-ocamlfuse -f -verbose "$HOME/gdrive"
-            fi
+            REQUEST_TOKEN="true"
         fi
+    else
+        printf "No access token found. Requesting new one...\n"
+        REQUEST_TOKEN="true"
     fi
 else
+    REQUEST_TOKEN="true"
+fi
+
+if [ "$REQUEST_TOKEN" = "true" ]; then
     if google-drive-ocamlfuse -headless -debug -log_to - -id "$CLIENT_ID" -secret "$SECRET"; then
-        #printf "Access token valid. Mounting filesystem on %s" "$HOME/gdrive"
         google-drive-ocamlfuse -f -verbose "$HOME/gdrive"
     fi
 fi
