459a8f60a203c51254049b58d1dad22229d48d58
Abel Martín <abel.martin.ruiz@gmail.com>
2024-05-21 16:50:24 +0200

    Improve entrypoint detection of expired tokens

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
-if [[ -f $HOME/.gdfuse/default/state ]]; then
-	google-drive-ocamlfuse -debug -log_to - $HOME/gdrive
+if [ -f "$HOME/.gdfuse/default/state" ]; then
+    printf "State file found. Checking access token validity...\n"
+    ACCESS_TOKEN=$(grep last_access_token "$HOME/.gdfuse/default/state" | sed -r "s/^last_access_token=//")
+    if [ -n "$ACCESS_TOKEN" ]; then
+        if wget "https://oauth2.googleapis.com/tokeninfo?access_token=${ACCESS_TOKEN}" -O /dev/null --quiet; then
+            printf "Access token valid. Mounting filesystem on %s.\n" "$HOME/gdrive"
+            google-drive-ocamlfuse -f -verbose "$HOME/gdrive"
+        else
+            printf "Access token invalid. Revalidating...\n"
+            rm -f "$HOME/.gdfuse/default/state"
+            if google-drive-ocamlfuse -headless -debug -log_to - -id "$CLIENT_ID" -secret "$SECRET"; then
+                google-drive-ocamlfuse -f -verbose "$HOME/gdrive"
+            fi
+        fi
+    fi
 else
-	google-drive-ocamlfuse -headless -id ${CLIENT_ID} -secret ${SECRET}
-
-	if [[ "$?" == "0" ]]; then
-		google-drive-ocamlfuse -debug -log_to - $HOME/gdrive
-	else
-		/bin/sh
-	fi
+    if google-drive-ocamlfuse -headless -debug -log_to - -id "$CLIENT_ID" -secret "$SECRET"; then
+        #printf "Access token valid. Mounting filesystem on %s" "$HOME/gdrive"
+        google-drive-ocamlfuse -f -verbose "$HOME/gdrive"
+    fi
 fi
